87bcbbe77057d4d2586ae58327b3440cc25d2a95
Winston Laoh <winston@gridspace.com>
2021-06-30 13:59:53 -0700

    better help message

--- piptools/scripts/compile.py
+++ piptools/scripts/compile.py
@@ -231,7 +231,8 @@ def _get_default_option(option_name: str) -> Any:
     "--write-deps",
     is_flag=True,
     default=False,
-    help="Write subdependencies of specified packages to output file.",
+    help="Write subdependencies of specified packages to output file. If not specified,"
+    "--allow-unsafe flag must be set.",
 )
 def cli(
     ctx: click.Context,
